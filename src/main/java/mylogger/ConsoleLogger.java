37e7be48804d381b6348a9d26c2306513e044a93
Raymond <raymond.yang121@outlook.com>
2023-07-25 00:42:41 -0400

    update logger

--- src/main/java/mylogger/ConsoleLogger.java
+++ src/main/java/mylogger/ConsoleLogger.java
@@ -14,10 +14,17 @@ public class ConsoleLogger {
         ConfigurationBuilder<BuiltConfiguration> builder
                 = ConfigurationBuilderFactory.newConfigurationBuilder();
 
-        builder.setStatusLevel(Level.ERROR);
-        builder.setConfigurationName("BuilderTest");
-        builder.add(builder.newAppender("Stdout", "CONSOLE").addAttribute("target",
-                org.apache.logging.log4j.core.appender.ConsoleAppender.Target.SYSTEM_OUT));
+        // Set the pattern for the log messages
+        //String pattern = "%d{yyyy-MM-dd HH:mm:ss} %-5level %class{36} %L %M - %msg%xEx%n";
+        String pattern = "%d{MM-dd HH:mm:ss} %-5level %class{36} %L %M - %msg%xEx%n";
+        builder.add(
+            builder.newAppender("Stdout", "CONSOLE")
+                .addAttribute("target", org.apache.logging.log4j.core.appender.ConsoleAppender.Target.SYSTEM_OUT)
+                .add(
+                    builder.newLayout("PatternLayout")
+                            .addAttribute("pattern", pattern)
+                )
+        );
         builder.add(builder.newRootLogger(Level.INFO).add(builder.newAppenderRef("Stdout")));
 
         ((LoggerContext) LogManager.getContext(false)).start(builder.build());
