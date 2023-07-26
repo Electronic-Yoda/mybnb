272a18c7ec8d6fc01e8c184612d018c76523acd8
Raymond <raymond.yang121@outlook.com>
2023-07-25 23:41:12 -0400

    change record Long type to long type, as all long types will never need to be null, change location of ServiceCli

--- src/main/java/domain/Booking.java
+++ src/main/java/domain/Booking.java
@@ -4,6 +4,6 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 // Domain record for the 'rentings' table
-public record Booking(Long booking_id, LocalDate start_date, LocalDate end_date, LocalDate transaction_date,
-                      BigDecimal amount, String currency, String payment_method, Long users_sin, Long listings_listing_id) {}
+public record Booking(long booking_id, LocalDate start_date, LocalDate end_date, LocalDate transaction_date,
+                      BigDecimal amount, String currency, String payment_method, long users_sin, long listings_listing_id) {}
 
--- src/main/java/tools/ServiceCli.java
+++ src/main/java/tools/ServiceCli.java
@@ -8,7 +8,11 @@ import org.apache.logging.log4j.Logger;
 public class ServiceCli {
     // Command Line Interface. Handles user input and output
     private static final Logger logger = LogManager.getLogger(ServiceCli.class);
-    private final Dao dao = new Dao();
+    private final Dao dao = new Dao(
+            "jdbc:mysql://localhost:3307/mydb",
+            "root",
+            "password"
+    );
     private final UserService userService = new UserService(dao);
     private final ListingService listingService = new ListingService(dao);
     private final BookingService bookingService = new BookingService(dao);
