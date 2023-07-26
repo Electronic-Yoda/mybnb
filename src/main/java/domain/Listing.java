c5912c0957caac8a85b0f90a97c74e28a6cab565
Raymond <raymond.yang121@outlook.com>
2023-07-26 02:06:03 -0400

    fix incorrect type in Listing record

--- src/main/java/domain/Listing.java
+++ src/main/java/domain/Listing.java
@@ -3,6 +3,7 @@ package domain;
 import java.math.BigDecimal;
 
 // Domain record for the 'listings' table
-public record Listing(long listing_id, String listing_type, BigDecimal price_per_night, BigDecimal longitude,
-                      String postal_code, BigDecimal latitude, String city,
-                      String country, String amenities, Long users_sin) {}
+public record Listing(long listing_id, String listing_type, BigDecimal price_per_night,
+                      String postal_code, BigDecimal longitude,
+                      BigDecimal latitude, String city,
+                      String country, String amenities, long users_sin) {}
